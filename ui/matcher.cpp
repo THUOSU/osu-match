d9b13e7584855a3e99dc4b99633b1546457ddb14
KotonoYuuri <tadanotorch@qq.com>
2015-01-14 00:17:38 +0800

    temporary match implementation

---
+++ ui/matcher.cpp
@@ -0,0 +1,41 @@
+#include "matcher.hpp"
+#include "filesystem.hpp"
+
+#include <exception>
+#include <system_error>
+
+using namespace thuosu::filesystem;
+
+inline static path mbak_path(const path & src)
+{
+	path mbak_file{ src };
+	mbak_file.replace_extension(mbak_file.extension() + L".mbak");
+	return mbak_file;
+}
+
+namespace thuosu
+{
+	bool has_matched(const path & music_file)
+	{
+		return is_regular_file(mbak_path(music_file));
+	}
+
+	void match_music(const path & src, const path & dst)
+	{
+		auto mbak_file = mbak_path(src);
+		if (!exists(mbak_file))
+			rename(src, mbak_file);
+
+		/////////////// not implemented ////////////////////
+		copy_file(dst, src, copy_option::overwrite_if_exists);
+	}
+
+	void recovery(const path & music_file)
+	{
+		path mbak_file{ music_file };
+		mbak_file.replace_extension(mbak_file.extension() + L".mbak");
+		if (exists(music_file))
+			remove(music_file);
+		rename(mbak_file, music_file);
+	}
+}
---
+++ ui/matcher.hpp
@@ -0,0 +1,15 @@
+#ifndef THUOSU_MATCHER_HPP
+#define THUOSU_MATCHER_HPP
+
+#include "filesystem.hpp"
+
+namespace thuosu
+{
+	bool has_matched(const filesystem::path & music_file);
+
+	void match_music(const filesystem::path & src, const filesystem::path & dst);
+
+	void recovery(const filesystem::path & music_file);
+}
+
+#endif // THUOSU_MATCHER_HPP
